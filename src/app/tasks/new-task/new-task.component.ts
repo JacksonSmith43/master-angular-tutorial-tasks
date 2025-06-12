10572286db284b994ee6a2bba6c5ef48ef82a059
JacksonSmith43 <jessica444@gmx.at>
2025-06-12 10:25:49 +0200

    177. Using Angular's Dependency Injection Mechanism.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -8,8 +9,12 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
+
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  constructor(private tasksService: TasksService) {
+  }
 
   onAddTask(title: string, description: string) {
     this.formEl()?.nativeElement.reset();
