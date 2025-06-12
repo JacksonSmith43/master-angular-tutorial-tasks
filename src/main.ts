54d6ce43a846eb6c35802fb2791ae3e3e2e5289b
JacksonSmith43 <jessica444@gmx.at>
2025-06-12 15:34:02 +0200

    181. There Are Multiple Ways Of Providing a Service.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err)); //  { providers: [TasksService] } replaces @Injectable({providedIn: "root", }) from within tasks.service.ts.
