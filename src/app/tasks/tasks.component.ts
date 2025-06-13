184d00c04c2dca295f0bd5bed6e91c45c19c6f29
JacksonSmith43 <jessica444@gmx.at>
2025-06-13 08:54:30 +0200

    183. Understanding the Element Injector's Behavior.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -8,7 +8,6 @@ import { TasksService } from './tasks.service';
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
-  imports: [NewTaskComponent, TasksListComponent],
-  providers: [TasksService] // Element injector. 
+  imports: [NewTaskComponent, TasksListComponent]
 })
 export class TasksComponent { }
